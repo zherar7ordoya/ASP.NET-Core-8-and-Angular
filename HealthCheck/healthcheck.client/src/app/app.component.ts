08ecad29b51d63af6b11d445d2cb60a49ca13df6
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-08-14 13:00:30 -0300

    VSCode

--- HealthCheck/healthcheck.client/src/app/app.component.ts
+++ HealthCheck/healthcheck.client/src/app/app.component.ts
@@ -2,36 +2,39 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+    date: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
+
 export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
 
-  constructor(private http: HttpClient) {}
+    public forecasts: WeatherForecast[] = [];
 
-  ngOnInit() {
-    this.getForecasts();
-  }
+    constructor(private http: HttpClient) { }
 
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
+    ngOnInit() {
+        this.getForecasts();
+    }
 
-  title = 'healthcheck.client';
+    getForecasts() {
+
+        this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
+            result => {
+                this.forecasts = result;
+            },
+            error => {
+                console.error(error);
+            }
+        );
+    }
+
+    title = 'healthcheck.client';
 }
--- HealthCheck/healthcheck.client/src/app/app.module.ts
+++ HealthCheck/healthcheck.client/src/app/app.module.ts
@@ -6,14 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
