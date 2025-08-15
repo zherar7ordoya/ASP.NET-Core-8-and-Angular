41a7e83c9d04307d1480f60584a73ea76b6f30ae
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-08-15 12:58:27 -0300

    Environment

--- HealthCheck/healthcheck.client/src/app/app.module.ts
+++ HealthCheck/healthcheck.client/src/app/app.module.ts
@@ -4,16 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        FetchDataComponent,
+        NavMenuComponent
+    ],
     imports: [
         BrowserModule,
         HttpClientModule,
         AppRoutingModule
     ],
     providers: [],
-    bootstrap: [AppComponent]
+    bootstrap: [
+        AppComponent
+    ]
 })
 
 export class AppModule { }
