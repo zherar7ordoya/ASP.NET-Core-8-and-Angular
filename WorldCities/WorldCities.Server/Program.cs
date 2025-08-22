a9783a1c83c6f5e457b9389d20ce1779b4082d3a
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-08-22 11:40:44 -0300

    EPPlus 8

--- WorldCities/WorldCities.Server/Program.cs
+++ WorldCities/WorldCities.Server/Program.cs
@@ -1,3 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+using OfficeOpenXml;
+
+using WorldCities.Server.Data;
+
+ExcelPackage.License.SetNonCommercialPersonal("Gerardo Tordoya");
+
+// === *** ===
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +16,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// === *** ===
+
+// Add ApplicationDbContext and SQL Server support 
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// === *** ===
 
 var app = builder.Build();
 
