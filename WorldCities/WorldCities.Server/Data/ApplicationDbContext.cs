a35ace574593bbee0277b852b1593fca2828ed97
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-08-23 02:05:03 -0300

    PS1

--- WorldCities/WorldCities.Server/Data/ApplicationDbContext.cs
+++ WorldCities/WorldCities.Server/Data/ApplicationDbContext.cs
@@ -1,13 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+
 using WorldCities.Server.Data.Models;
 
-namespace WorldCities.Server.Data;
-
-public class ApplicationDbContext : DbContext
+namespace WorldCities.Server.Data
 {
-    public ApplicationDbContext() : base()    {    }
-    public ApplicationDbContext(DbContextOptions options)     : base(options)    {    }
+    public class ApplicationDbContext : DbContext
+    {
 
-    public DbSet<City> Cities => Set<City>();
-    public DbSet<Country> Countries => Set<Country>();
+        public ApplicationDbContext() : base()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+         : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // add the EntityTypeConfiguration classes
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(ApplicationDbContext).Assembly
+                );
+        }
+
+        public DbSet<City> Cities => Set<City>();
+        public DbSet<Country> Countries => Set<Country>();
+
+    }
 }
